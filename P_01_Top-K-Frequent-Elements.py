22524f698a380138de453377e18b2e3d35c556b2
Prashant <juugi202412961@jainuniversity.ac.in>
2025-12-20 13:30:24 +0530

    P_01_Top-K-Frqueent-Element is solved

---
+++ P_01_Top-K-Frequent-Elements.py
@@ -0,0 +1,19 @@
+from collections import Counter
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        freq = Counter(nums)
+        buck = [ [] for _ in range(len(nums) + 1 )]
+
+        for num,count in freq.items(): 
+          buck[count].append(num)
+    
+        result = []
+        for count in range(len(nums), 0, -1):  
+           for num in buck[count]:
+              result.append(num)
+              if len(result) == k:
+                 return result
+
+nums = [1,1,1,2,2,3]
+k = 2
+print(Solution().topKFrequent(nums, k))
