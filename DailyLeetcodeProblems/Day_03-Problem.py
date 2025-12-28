e104f01f0d3b593475a86fcbe87fcad147d5bfb4
Prashant <juugi202412961@jainuniversity.ac.in>
2025-12-28 19:27:01 +0530

    Solved Day 03 Problem of Count Negative numbers in a sorted matrix

---
+++ DailyLeetcodeProblems/Day_03-Problem.py
@@ -0,0 +1,14 @@
+# COUNT NEGATIVE NUMBERS IN A SORTED MATRIX :
+class Solution(object): 
+  def countNegatives(self, grid): 
+    count= 0
+    for row in grid: 
+      for element in row: 
+         if element < 0: 
+            count = count + 1 
+    return count 
+
+grid = [[3,2],[1,0]]
+sol = Solution()
+last = sol.countNegatives(grid)
+print(last)
