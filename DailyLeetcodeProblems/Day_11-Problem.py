fcfe3645265f501e2c311bacbddc52450091d3fe
Prashant <juugi202412961@jainuniversity.ac.in>
2026-01-06 18:51:36 +0530

    Day 10 medium problem solved about Maximum Level sum of a binary tree

---
+++ DailyLeetcodeProblems/Day_11-Problem.py
@@ -0,0 +1,84 @@
+#! 1161. Maximum Level Sum of a Binary Tree
+
+# Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
+# Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
+
+# Example 1:
+
+# Input: root = [1,7,0,7,-8,null,null]
+# Output: 2
+# Explanation: 
+# Level 1 sum = 1.
+# Level 2 sum = 7 + 0 = 7.
+# Level 3 sum = 7 + -8 = -1.
+# So we return the level with the maximum sum which is level 2.
+
+# Example 2:
+# Input: root = [989,null,10250,98693,-89388,null,null,null,-32127]
+# Output: 2
+ 
+ 
+# Constraints:
+# The number of nodes in the tree is in the range [1, 104].
+# -105 <= Node.val <= 105
+
+#Code : 
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def maxLevelSum(self, root):
+        if not root : return 0 
+
+        q= [root]
+        max_level = 1 
+        level = 1 
+        max_sum = float('-inf') 
+
+        while q: 
+            level_sum = 0 
+            next_level = []
+            
+            for node in q : 
+              level_sum += node.val
+              
+              if node.left: 
+                next_level.append(node.left)
+              if node.right: 
+                next_level.append(node.right)
+            
+            if level_sum > max_sum: 
+              max_sum = level_sum
+              max_level = level 
+            
+            q= next_level
+            level += 1  
+        return max_level
+        
+def build_tree(lst):
+    if not lst:
+        return None
+    root = TreeNode(lst[0])
+    queue = [root]
+    i = 1
+    while i < len(lst):
+        node = queue.pop(0)
+        if lst[i] != 'null':  # if the value is not null, create a left child
+            node.left = TreeNode(lst[i])
+            queue.append(node.left)
+        i += 1
+        if i < len(lst) and lst[i] != 'null':  # if the value is not null, create a right child
+            node.right = TreeNode(lst[i])
+            queue.append(node.right)
+        i += 1
+    return root
+
+root = [1, 7, 0, 7, -8, 'null', 'null']
+sol = Solution()
+tree_root = build_tree(root)
+last = sol.maxLevelSum(tree_root)
+print(last)
+
+            
