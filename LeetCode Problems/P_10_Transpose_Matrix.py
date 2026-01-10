b0e54ee7384e25cd9ddf57a832e3dba877d52b4f
Prashant <juugi202412961@jainuniversity.ac.in>
2026-01-10 19:12:11 +0530

    Solved tenth Problem easy one which was about Transpose a Matrix

---
+++ LeetCode Problems/P_10_Transpose_Matrix.py
@@ -0,0 +1,13 @@
+#! Transpose a Matrix : 
+
+class Solution(object):
+    def transpose(self, matrix):
+        transpose_row = zip(*matrix)
+        transpose_matrix = [list(row) for row in transpose_row]
+        return transpose_matrix
+
+
+matrix =[[1,2,3],[4,5,6],[7,8,9]]      
+sol = Solution() 
+value = sol.transpose(matrix)
+print(value)
