1efe82548a2eff2ead04f4ddf5fe5f397104f468
Prashant <juugi202412961@jainuniversity.ac.in>
2026-01-21 21:10:00 +0530

    More on Array is commited

--- DSA with APNA College C++/PR_2Array.cpp
+++ DSA with APNA College C++/PR_2Array.cpp
@@ -83,20 +83,106 @@
 
 
 // -----------------------------------------------------------------------------
+// PRACTICE QUESTIONS TO SOLVE : 
+
+// ! 1 : SUM AND PRODUCT OF ALL NUMBERS IN AN ARRAY 
+// #include <iostream> 
+// using namespace std ; 
 
 
+// int sum_product (int arr[] , int sz){
+//   int sum = 0 ; 
+//   int product = 1 ; 
+//    for (int i=0 ; i<sz ; i++){ 
+//          sum += arr[i] ; 
+//          product *= arr[i] ; 
+//    }
+//   cout << "Sum of all Numbers is : "<<  sum  << endl; 
+//   return  product ;
+// }
+// int main(){ 
+//   int sz = 5 ; 
+//   int arr[] = {1,2,3,4,5}; 
+//   cout << sum_product(arr,sz);
+ 
+
+// }
+
+
+// -----------------------------------------------------------------------------
+
+// SWAP MIN AND MAX NUMBERS IN ARRAY : O(N)
+
+// #include <iostream> 
+// using namespace std ; 
+// void find(int arr[] ,int sz ){ 
+//     int smallest = arr[0] ;
+//     int largest  = arr[0];
+//     for(int i=0 ; i<sz ; i++){ 
+//         smallest = min(smallest , arr[i]);
+//         largest = max(largest , arr[i]);
+//     }
+//   int value=1 ; 
+//   int value2=1 ;
+//    for (int i=0 ; i<sz ; i++){ 
+//         if (smallest == arr[i]){ 
+//             value  = i ; 
+//         }
+//         if (largest == arr[i]){ 
+//              value2 = i ; 
+//         }
+//    }
+//         int temp = arr[value];
+//         arr[value] = arr[value2]; 
+//         arr[value2] =temp ; 
+//         cout << arr[value] << endl ;
+//         cout << arr[value2] << endl;
+
+//   }
+
+
+
+// int main(){ 
+//     int sz =5 ; 
+//     int arr[] = {1,2,3,4,5}; 
+
+//    find(arr,sz);
+//    for(int i=0 ;i<sz ;i++){ 
+//       cout<< arr[i] << " " ; 
+//    }
+// }
+
+
+// -----------------------------------------------------------------------------
+
+//! UNIQUE VALUES IN AN ARRAY : 
 
 #include <iostream> 
 using namespace std ; 
 
 int main(){ 
-  
+   int sz = 6 ; 
+   int arr[] = {1,1,2,3,2,4,5};
+   std::vector<int> arr2;
+   for (int i=1 ; i<sz ; i++){ 
+      if (arr[i] != arr[i-1]){ 
+          arr2.push_back(arr[i]) ;
+      }
+      else { 
+         continue ; 
+      }
+   }
+   for (int i=0 ; i<sizeof(arr2)/sizeof(int) ; i++){
+       cout << arr2[i] ; 
+   }
+
 }
 
 
 
 
 
+// -----------------------------------------------------------------------------
 
 
 
@@ -114,14 +200,7 @@ int main(){
 
 
 
-
-
-// #include <iostream> 
-// using namespace std ; 
-
-// int main(){ 
-  
-// }
+// -----------------------------------------------------------------------------
 
 
 
@@ -133,23 +212,3 @@ int main(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// #include <iostream> 
-// using namespace std ; 
-
-// int main(){ 
-  
-// }
