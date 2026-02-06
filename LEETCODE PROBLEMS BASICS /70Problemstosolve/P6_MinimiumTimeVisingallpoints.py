f47a480aaa4c197d9ec7767c580fc51b0920f080
Prashant <juugi202412961@jainuniversity.ac.in>
2026-02-06 10:08:11 +0530

    Solved minimum time visiting all points

--- LEETCODE PROBLEMS BASICS /70Problemstosolve/P6_MinimiumTimeVisingallpoints.py
+++ LEETCODE PROBLEMS BASICS /70Problemstosolve/P6_MinimiumTimeVisingallpoints.py
@@ -1,7 +1,8 @@
 #! Minimium Time Visiting all points 
 def MinimiumTime(points):
   result = 0
-  x1 , y1 = points.pop()
+  x1 , y1 = points.pop(0)
+  print(x1,y1)
   while points: 
     x2 ,y2 = points.pop()
     result += max(abs(y2-y1),abs(x2-x1))
