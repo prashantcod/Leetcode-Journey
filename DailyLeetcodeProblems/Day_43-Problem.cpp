c14fe67537aad554d1855d25e9b85662f12e8d6e
Prashant <juugi202412961@jainuniversity.ac.in>
2026-02-14 19:28:29 +0530

    Day 43 Problem solved which was about Champagne Tower

---
+++ DailyLeetcodeProblems/Day_43-Problem.cpp
@@ -0,0 +1,28 @@
+// 799. Champagne Tower
+// 
+
+// We stack glasses in a pyramid, where the first row has 1 glass, the second row has 2 glasses, and so on until the 100th row.  Each glass holds one cup of champagne.
+
+// Then, some champagne is poured into the first glass at the top.  When the topmost glass is full, any excess liquid poured will fall equally to the glass immediately to the left and right of it.  When those glasses become full, any excess champagne will fall equally to the left and right of those glasses, and so on.  (A glass at the bottom row has its excess champagne fall on the floor.)
+
+// For example, after one cup of champagne is poured, the top most glass is full.  After two cups of champagne are poured, the two glasses on the second row are half full.  After three cups of champagne are poured, those two cups become full - there are 3 full glasses total now.  After four cups of champagne are poured, the third row has the middle glass half full, and the two outside glasses are a quarter full, as pictured below.
+
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        double tower[102][102] = {0.0};
+        tower[0][0] = (double)poured;
+        
+        for (int r = 0; r <= query_row; r++) {
+            for (int c = 0; c <= r; c++) {
+                if (tower[r][c] > 1.0) {
+                    double excess = (tower[r][c] - 1.0) / 2.0;
+                    tower[r][c] = 1.0;
+                    tower[r + 1][c] += excess;
+                    tower[r + 1][c + 1] += excess;
+                }
+            }
+        }
+        return tower[query_row][query_glass];
+    }
+};
